03322acbd1e76cf891c2324b4b280c1e4c10a661
gabrielgroppo123 <gabgroppo@gmail.com>
2025-08-11 09:25:29 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,14 @@
+import prompt from "prompt-sync"
+const ler = prompt ();
+
+console.log("Tabuada")
+
+console.log("Informe o número:");
+let n1= Number(ler());
+
+console.log("Informe até onde deseja a tabuada:");
+let n2= Number(ler());
+
+for(let i = 0; i <= n2; i++) {
+     console.log(n1 + " x " + i + " = " + (n1 * i))
+    }
