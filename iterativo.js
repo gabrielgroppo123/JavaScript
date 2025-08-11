fc6eab78b7fc910a1e2dfa0e8976d9e24a881e88
gabrielgroppo123 <gabgroppo@gmail.com>
2025-08-11 09:16:04 -0300

    Add files via upload

---
+++ iterativo.js
@@ -0,0 +1,18 @@
+let contador = 1;
+
+while(contador <= 10) {
+    console.log (contador);
+
+    contador++;
+}
+
+do {
+    console.log (contador);
+
+    contador++;
+}
+while(contador <= 10);
+
+for(let contador = 1; contador <=5; contador++){
+    
+}
